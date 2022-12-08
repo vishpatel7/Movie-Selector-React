184ab547bc52629bd54d764ed9d7ff2e66d9e11d
Vish Patel <vpatel@and.digital>
2022-12-08 16:48:09 +0000

    added replacement / fixed images

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,13 +3,10 @@ import PropTypes from "prop-types";
 
 import "../styles/MovieList.css";
 
-const DEFAULT_POSTER_SRC =
-  "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-768x1129.jpg";
-
 const MovieCard = (props) => {
   return (
     <div className="image-container d-flex movie-frame justify-content-start p-0 m-3">
-      <img src={props.posterSrc || DEFAULT_POSTER_SRC} alt="movie" />
+      <img src={props.posterSrc} height={420} alt="movie" />
       {props.footerAction ? (
         <div className="overlay d-flex align-items-center justify-content">
           {props.footerAction}
--- src/utils/searchMovies.js
+++ src/utils/searchMovies.js
@@ -1,3 +1,6 @@
+const DEFAULT_POSTER_SRC =
+  "https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie-768x1129.jpg";
+
 const searchMovies = async (query) => {
   const url = `http://www.omdbapi.com/?s=${query}&apikey=ddd1d67b`;
   const response = await fetch(url);
@@ -8,7 +11,9 @@ const searchMovies = async (query) => {
     title: result.Title,
     year: result.Year,
     type: result.Type,
-    posterSrc: result.Poster,
+    posterSrc: result.Poster.startsWith("http")
+      ? result.Poster
+      : DEFAULT_POSTER_SRC,
   }));
 
   return {
